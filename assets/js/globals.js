981a24f80b6ccdd675eb5e56cfb02ad26f93a3cb
I am Billie <dosbor3@gmail.com>
2021-12-18 09:53:02 -0600

    globals.js created to store global variables

---
+++ assets/js/globals.js
@@ -0,0 +1,13 @@
+var score = 0;
+var highScore = 0;
+var userInitials = "";
+var timer = 0;
+var timeRemaining = 0;
+var questionObj = {
+    question: "",
+    answerChoice1: "",
+    answerChoice2: "",
+    answerChoice3: "",
+    answerChoice4: "",
+    correctanswer: ""
+};
