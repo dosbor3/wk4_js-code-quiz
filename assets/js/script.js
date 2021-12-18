5dc68c75e9f3f57746a44cda88deacb07d6bbf44
I am Billie <dosbor3@gmail.com>
2021-12-18 15:16:54 -0600

    updated globals.js and script.js

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+startBtn.addEventListener("click", function(event) {
+    console.log("It works!")
+    startTimer();
+
+}) 
+
+function startTimer() {
+    timer = setInterval(function() {
+        console.log(timeRemaining);
+        timeRemaining --;
+    }, 1000)
+
+    if(timeRemaining <= 0) {
+        stopTimer();
+    }
+}
+
+function stopTimer(timer){
+    clearInterval(timer);
+}
+
+function displayQuestion() {
+    
+}
